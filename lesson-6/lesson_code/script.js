64c40bdebfe2c8f46cccd9f416aa055605ae3031
Henrik Rank <rank.henrik@gmail.com>
2022-10-20 17:33:00 +0300

    Update script.js

--- lesson-6/lesson_code/script.js
+++ lesson-6/lesson_code/script.js
@@ -23,14 +23,51 @@ console.log('Püramiidi pindala on ' + S + ' ja ruumala on ' + V + '.'); */
 
 // nameCut();
 
-var alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,š,z,ž,t,u,v,w,õ,ä,ö,ü,x,y"
+// var alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,š,z,ž,t,u,v,w,õ,ä,ö,ü,x,y"
 
-var r = alphabet.charAt(34);
-var i = alphabet.charAt(16);
-var c = alphabet.charAt(4);
-var h = alphabet.charAt(14);
-var a = alphabet.charAt(0);
-var r = alphabet.charAt(34);
-var d = alphabet.charAt(6);
+// var r = alphabet.charAt(34);
+// var i = alphabet.charAt(16);
+// var c = alphabet.charAt(4);
+// var h = alphabet.charAt(14);
+// var a = alphabet.charAt(0);
+// var r = alphabet.charAt(34);
+// var d = alphabet.charAt(6);
 
-document.write(r + i + c + h + a + r + d);
+// document.write(r + i + c + h + a + r + d);
+
+// for (var muutuja = 1; muutuja <= 100; muutuja++) {
+//     console.log(muutuja)
+// }
+
+// var num = 8;
+// num++;
+
+// document.write(num); // prints 9
+// Ülesanne 7
+// for (var i = 100; i >= 1; i-- ) {
+//     document.write(i)
+//     document.write('<br>')
+// }
+
+// ÜL 9
+// for (var i = 0; i <100; i++) {
+//     console.log('I am Bart Simpson and I like to write stuff on the blackboard');
+//     console.log('<br>')
+//     console.log(i)
+// }
+
+// Ülesanne 10
+// var myNameComponents = ["Gertrud", "Lukas"];
+// console.log(myNameComponents.join(" "));
+
+// Ülesanne 11
+
+// var kokku = 0;
+// var grades = [ 3, 4, 1, 5, 2, 4, 3, 2, 5, 5,5,5,5,5,5,5,5,5,5,5]
+// for ( var i = 0; i < grades.length; i++) {
+//     kokku = kokku + grades[i];
+// }
+
+// var averageGrade = kokku / grades.length;
+
+// console.log(averageGrade)
