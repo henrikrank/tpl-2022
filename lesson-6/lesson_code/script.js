690e2853ad8e236d671d879a0ff164e290174447
Henrik Rank <rank.henrik@gmail.com>
2022-10-20 16:05:11 +0300

    feat: last lesson code.

---
+++ lesson-6/lesson_code/script.js
@@ -0,0 +1,36 @@
+/* var a = 1;
+var h = 1;
+
+var S = 1 / 3 * a * h;
+var V = 1 / 2 * a * h;
+
+S = S.toFixed(2);
+console.log('Püramiidi pindala on ' + S + ' ja ruumala on ' + V + '.'); */
+
+// function nameCut() { 
+
+//     var myName = window.prompt("Enter your name:")
+
+//     if (myName.length >= 11  ) {
+//         myName = myName.slice(0, 11)
+//         alert(myName + "...")
+//     }
+    
+//     else {
+//         alert(myName)
+//     }
+// }
+
+// nameCut();
+
+var alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,š,z,ž,t,u,v,w,õ,ä,ö,ü,x,y"
+
+var r = alphabet.charAt(34);
+var i = alphabet.charAt(16);
+var c = alphabet.charAt(4);
+var h = alphabet.charAt(14);
+var a = alphabet.charAt(0);
+var r = alphabet.charAt(34);
+var d = alphabet.charAt(6);
+
+document.write(r + i + c + h + a + r + d);
